3190829675dbe7bf4c5a6fa3f2c1982c3a7c6aa9
nikitamessi69 <nzhuravlev92@gmail.com>
2022-12-20 17:41:49 +0300

    Добавил решение задачи 2

--- Task_2/Program.cs
+++ Task_2/Program.cs
@@ -3,4 +3,16 @@
 //a = 2 b = 10 -> max = 10
 //a = -9 b = -3 -> max = -3
 
+Console.WriteLine("Введите первое число: ");
+int a = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите первое число: ");
+int b = int.Parse(Console.ReadLine()!);
 
+if (a>b)
+{
+    Console.WriteLine("Первое число " + a + " больше, чем второе " + b);
+}
+else
+{
+    Console.WriteLine("Второе число " + b + " больше, чем первое " + a);
+}
