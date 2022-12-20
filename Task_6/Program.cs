c6df77c2c48d235097c9182636e09358a53671a3
nikitamessi69 <nzhuravlev92@gmail.com>
2022-12-20 19:00:31 +0300

    Добавил решение задачи 6

--- Task_6/Program.cs
+++ Task_6/Program.cs
@@ -3,3 +3,14 @@
 //-3 -> нет
 //7 -> нет
 
+Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num % 2 == 0)
+{
+    Console.WriteLine("Четное");
+}
+else
+{
+    Console.WriteLine("Нечетное");
+}
