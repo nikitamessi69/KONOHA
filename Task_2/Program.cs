df54312a83301620768e0a8e9cefc17758c29611
nikitamessi69 <nzhuravlev92@gmail.com>
2022-12-20 18:06:20 +0300

    Добавил решение задачи 4 и поправил задачу 2

--- Task_2/Program.cs
+++ Task_2/Program.cs
@@ -5,7 +5,7 @@
 
 Console.WriteLine("Введите первое число: ");
 int a = int.Parse(Console.ReadLine()!);
-Console.WriteLine("Введите первое число: ");
+Console.WriteLine("Введите второе число: ");
 int b = int.Parse(Console.ReadLine()!);
 
 if (a>b)
--- Task_4/Program.cs
+++ Task_4/Program.cs
@@ -3,3 +3,22 @@
 //44 5 78 -> 78
 //22 3 9 -> 22
 
+Console.WriteLine("Введите первое число: ");
+int n_1 = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите второе число: ");
+int n_2 = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите третье число: ");
+int n_3 = int.Parse(Console.ReadLine()!);
+
+if (n_1 > n_2 && n_1 > n_3)
+{
+    Console.WriteLine("Большее число = " + n_1);
+}
+else if (n_2 > n_3)
+{
+    Console.WriteLine("Большее число = " + n_2);
+}
+else
+{
+    Console.WriteLine("Большее число = " + n_3);
+}
