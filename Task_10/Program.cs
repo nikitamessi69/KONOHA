7813f0804f932fe55a4574523f31ee230aa9cdb6
nikitamessi69 <nzhuravlev92@gmail.com>
2022-12-26 15:55:56 +0300

    Добавил решение задачи 15

--- Task_10/Program.cs
+++ Task_10/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+456 -> 5
+782 -> 8
+918 -> 1
+*/
+
+int number = Console.WriteLine("Введите трехзначное число: ");
+int amount = number.ToString().Length;
+
+if (amount < 3 || amount > 3)
+{
+    Console.WriteLine("Вы ввели не трехзначное число");
+}
+else
+{
+    Console.WriteLine(InCenter(number));
+}
--- Task_15/Program.cs
+++ Task_15/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+6 -> да
+7 -> да
+1 -> нет
+*/
+
+Console.WriteLine("Введите число от 1 до 7: ");
+int HolNum = Convert.ToInt32(Console.ReadLine());
+
+if (HolNum >= 1 && HolNum <= 7)
+{
+    if (HolNum == 6 || HolNum == 7) 
+    {
+        Console.WriteLine("Выходной день");
+    }
+    else 
+    {
+        Console.WriteLine("Будний день");
+    }
+}    
