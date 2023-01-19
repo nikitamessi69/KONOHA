dc10bc11c4ad44cdfbc2fc5b604bb6bc3ffb5cf8
nikitamessi69 <nzhuravlev92@gmail.com>
2023-01-19 19:55:14 +0300

    Добавил решения домашки 4 семинара

---
+++ Task_25/Program.cs
@@ -0,0 +1,18 @@
+﻿/*
+Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+3, 5 -> 243 (3⁵)
+2, 4 -> 16
+*/
+
+int numA, numB, degree;
+Console.Write("Ввести число А: ");
+numA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Ввести число B: ");
+numB = Convert.ToInt32(Console.ReadLine());
+
+degree = numA;
+for(int i = 2; i <= numB; i++)
+{
+    degree *= numA;
+}
+ Console.WriteLine($"{numA}^{numB} = {degree}");
---
+++ Task_29/Program.cs
@@ -0,0 +1,19 @@
+﻿/*
+Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+6, 1, 33 -> [6, 1, 33]
+*/
+
+int getNumber;
+Console.WriteLine("Будет создан массив");
+Console.Write("Введите любое число: ");
+getNumber = Convert.ToInt32(Console.ReadLine());
+
+int[] array = new int[getNumber];
+
+for (int i = 0; i < getNumber; i++)
+{
+    array[i] = new Random().Next(getNumber+1);
+    Console.Write(array[i] + ", ");
+}
+Console.WriteLine();
