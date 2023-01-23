e42d6253343f8d0c11f024ca5603cdb970bd76c7
nikitamessi69 <nzhuravlev92@gmail.com>
2023-01-23 19:57:46 +0300

    Добавил пешения задач

--- Task_36/Program.cs
+++ Task_36/Program.cs
@@ -3,3 +3,35 @@
 [3, 7, 23, 12] -> 19
 [-4, -6, 89, 6] -> 0
 */
+
+Console.WriteLine("Ввести число для размера массива");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] numbers = new int[size];
+createArrayNum(numbers);
+printArray(numbers);
+int sum = 0;
+
+void createArrayNum(int[] numbers)
+{
+    for(int i = 0; i < numbers.Length; i++)
+        {
+            numbers[i] = new Random().Next(-30,30);
+        }
+}
+void printArray(int[] numbers)
+{
+    Console.Write("[");
+    for(int i = 0; i < numbers.Length; i++)
+        {
+            Console.Write(numbers[i] );
+            if (i < numbers.Length - 1);
+            {
+                Console.Write(", ");
+            }
+        }
+    Console.WriteLine("]");
+}
+for (int i = 0; i < numbers.Length; i += 2)
+    sum += numbers[i];
+
+    Console.WriteLine($"У нас {numbers.Length} нечетных числа, сумма чисел = {sum}");
