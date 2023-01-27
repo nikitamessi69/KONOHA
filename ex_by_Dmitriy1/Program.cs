2d572b0c9bd05b39794f9aa26a53858df90f6d67
nikitamessi69 <nzhuravlev92@gmail.com>
2023-01-27 15:31:31 +0300

    Новые условия и решения

--- ex_by_Dmitriy1/Program.cs
+++ ex_by_Dmitriy1/Program.cs
@@ -18,12 +18,13 @@ void printInColor(string data)
 double[,] get2dDoubleArray(int colLength, int rowLength, int start, int finish)
 {
     double[,] array = new double [colLength, rowLength];
-    int divider = 100;
+    //int divider = 100;
     for (int i = 0; i < colLength; i++)
     {
         for (int j = 0; j < rowLength; j++)
         {
-            array[i,j] = new Random().Next(start * divider, (finish+1) * divider) / (divider + 0.0);
+            //array[i,j] = new Random().Next(start * divider, (finish+1) * divider) / (divider + 0.0);
+            array[i,j] = new Random().Next(start, finish+1) + Math.Round(new Random().NextDouble(),2);
         }
     }
     return array;
