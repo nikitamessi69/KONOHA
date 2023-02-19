1c10a5cb4b91e060cc692aa156e90859970ac91e
nikitamessi69 <nzhuravlev92@gmail.com>
2023-02-19 21:32:11 +0300

    Решения задач 64, 66, 68

--- Task_66/Program.cs
+++ Task_66/Program.cs
@@ -3,3 +3,26 @@
 M = 1; N = 15 -> 120
 M = 4; N = 8. -> 30
 */
+
+
+int SumMandN(int M, int N)
+{
+    int result = M;
+    if (M == N) return 0;
+    else
+    {
+        M++;
+        result = M + SumMandN(M, N); return result;
+    }
+}
+
+void SumCall(int M, int N)
+{
+    Console.Write(SumMandN(M - 1, N));
+}
+
+Console.Write("Введите число M: ");
+int M = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+SumCall(M, N);
--- Task_68/Program.cs
+++ Task_68/Program.cs
@@ -3,3 +3,30 @@
 m = 2, n = 3 -> A(m,n) = 9
 m = 3, n = 2 -> A(m,n) = 29
 */
+
+void AkkermanFunc(int m, int n)
+{
+    Console.Write(Function(m, n));
+}
+
+int Function(int m, int n)
+{
+    if (m == 0)
+    {
+        return n + 1;
+    }
+    else if (n == 0 && m > 0)
+    {
+        return Function(m - 1, 1);
+    }
+    else
+    {
+        return (Function(m - 1, Function(m, n - 1)));
+    }
+}
+
+Console.Write("Введите число m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+AkkermanFunc(m, n);
